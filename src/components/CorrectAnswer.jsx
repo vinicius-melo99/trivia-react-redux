1e32372887d4c504049e9fd59867dfe7d4bfa5fb
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2023-02-11 14:33:48 -0300

    finalizado-requisito-07

--- src/components/CorrectAnswer.jsx
+++ src/components/CorrectAnswer.jsx
@@ -1,12 +1,25 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import '../style/TelaGame.css';
 
 export default class CorrectAnswer extends Component {
+  selectCSS = () => {
+    const btnCorrect = document.querySelector('#defaultCorrect');
+    const btnWrong = document.querySelectorAll('#defaultWrong');
+    btnCorrect.className = 'correct-answer';
+    btnWrong.forEach((btn) => {
+      btn.className = 'wrong-answer';
+    });
+  };
+
   render() {
     const { resposta } = this.props;
     return (
       <button
+        type="button"
         data-testid="correct-answer"
+        onClick={ () => (this.selectCSS()) }
+        id="defaultCorrect"
       >
         { resposta }
       </button>
--- src/components/WrongAnswer.jsx
+++ src/components/WrongAnswer.jsx
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import '../style/TelaGame.css';
 
 export default class WrongAnswer extends Component {
+  selectCSS = () => {
+    const btnCorrect = document.querySelector('#defaultCorrect');
+    const btnWrong = document.querySelectorAll('#defaultWrong');
+    btnCorrect.className = 'correct-answer';
+    btnWrong.forEach((btn) => {
+      btn.className = 'wrong-answer';
+    });
+  };
+
   render() {
     const { indexTestId, resposta } = this.props;
     return (
       <button
         data-testid={ `wrong-answer-${indexTestId}` }
+        onClick={ () => (this.selectCSS()) }
+        id="defaultWrong"
       >
         { resposta }
       </button>
