822a5a2f4f7ae86c2fa73ed93d727fb56182af56
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2023-02-13 20:27:28 -0300

    finalizado-requisito-15

--- src/pages/FeedbackPage.jsx
+++ src/pages/FeedbackPage.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class FeedbackPage extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <h1 data-testid="feedback-text">Feedback</h1>
+      <>
+        <h1 data-testid="feedback-text">Feedback</h1>
+        <div>
+          <button
+            type="button"
+            data-testid="btn-play-again"
+            onClick={ () => history.push('/') }
+          >
+            Play Again
+          </button>
+        </div>
+        ;
+      </>
     );
   }
 }
 
-export default FeedbackPage;
+FeedbackPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default (FeedbackPage);
