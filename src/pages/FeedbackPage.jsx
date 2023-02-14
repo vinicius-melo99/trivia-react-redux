e67e8f61b980b6697c5ac74dd8b35b108b3a6457
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2023-02-14 10:32:35 -0300

    finalizado-requisito-16

--- src/pages/FeedbackPage.jsx
+++ src/pages/FeedbackPage.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class FeedbackPage extends Component {
   render() {
     return (
-      <h1 data-testid="feedback-text">Feedback</h1>
+      <>
+        <h1 data-testid="feedback-text">Feedback</h1>
+        <div>
+          <button
+            data-testid="btn-ranking"
+            type="button"
+            onClick={ () => {
+              const { history } = this.props;
+              history.push('/ranking');
+            } }
+          >
+            Ranking
+          </button>
+        </div>
+
+      </>
     );
   }
 }
+
+FeedbackPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default FeedbackPage;
---
+++ src/pages/TelaRanking.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TelaRanking extends Component {
+  render() {
+    return (
+      <div
+        data-testid="ranking-title"
+      >
+        <h1>TelaRanking</h1>
+
+      </div>
+    );
+  }
+}
+
+export default TelaRanking;
